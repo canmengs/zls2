617edd046b4d4e32fe98f4368da04b5a3d588e89
canmengs <32267872+canmengs@users.noreply.github.com>
2017-10-19 19:11:25 +0800

    zls

--- www/views/shop/shop-edit.controller.js
+++ www/views/shop/shop-edit.controller.js
@@ -1,16 +1,16 @@
-(function () {
+﻿(function () {
   'use strict';
   angular.module('starter.controllers').controller('ShopEditCtrl',['$scope','$stateParams','localStorageService',function ($scope,$stateParams,localStorageService) {
     $scope.title = $stateParams.title;
     $scope.property = $stateParams.property;
     $scope.shop = localStorageService.get('Shop',{
-      name:'刘智勇',
+      name:'张凌圣',
       ab:'',
-      name1:'刘智勇',
-      phone:'18650327647',
-      phone1:'18650327647',
+      name1:'张凌圣',
+      phone:'13003802381',
+      phone1:'13003802381',
       createTime:'2017-9-28 15:30:00',
-      email:'690284618@qq.com',
+      email:'805769602@qq.com',
       hylx:'111'
 
     });
